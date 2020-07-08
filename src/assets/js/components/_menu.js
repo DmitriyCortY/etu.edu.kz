99eb6867027fd2712e761c4e7f94b70bc5c41ec1
DmitriyCortY <dimaterlecskiu@gmail.com>
2020-07-08 19:37:03 +0300

    to home

--- src/assets/js/components/_menu.js
+++ src/assets/js/components/_menu.js
@@ -1,9 +1,10 @@
 class Menu {
     constructor(c = {}) {
-        this.trigger = c.trigger || $('.b-menu-trigger') // jQuery.object
+        this.trigger = c.trigger || $('[data-menu-trigger]') // jQuery.object
         this.header = c.header || $('.b-header') // jQuery.object
         this.t = 650
         this.status = false
+        this.flag = true
         this.menu = c.menu || $('.b-header__menu') // jQuery.object
 
         this.start()
@@ -14,10 +15,14 @@ class Menu {
     }
     listener() {
         this.trigger.on('click', e => {
-            this.handler_click()
+
+            if (this.flag) {
+                this.flag = false
+                this.handler_click()
+            }
         })
     }
-    handler_click() {
+    async handler_click() {
         $('body').toggleClass('_ov-h')
         if (!this.status) {
             this.trigger.toggleClass('active')
@@ -28,6 +33,7 @@ class Menu {
             setTimeout(() => {
                 this.menu.slideDown(this.t)
                 this.header.toggleClass('active_3')
+                this.flag = true
             }, this.t * 2)
             this.status = !this.status
         } else {
@@ -39,10 +45,10 @@ class Menu {
             setTimeout(() => {
                 this.header.toggleClass('active_1')
                 this.trigger.toggleClass('active')
+                this.flag = true
             }, this.t * 2)
             this.status = !this.status
         }
-
     }
 }
 
