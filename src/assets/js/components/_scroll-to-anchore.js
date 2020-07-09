bf8af63bb4fdec7904adb97ee3e1a0b6c2169929
DmitriyCortY <dimaterlecskiu@gmail.com>
2020-07-09 20:27:51 +0300

    to home

---
+++ src/assets/js/components/_scroll-to-anchore.js
@@ -0,0 +1,24 @@
+class Scroll_to_anchore {
+    constructor() {
+        this.trigger = $('a')
+        this.t = 666
+
+        this.listener()
+    }
+    listener() {
+        this.trigger.on('click', e => {
+            let t = $(e.target)
+            if (t.attr('href').includes('#')) {
+                e.preventDefault();
+                let name = $(e.target).attr('href').replace('#', '')
+                this.scroll_to(name)
+            }
+        })
+    }
+    scroll_to(name) {
+        $('html,body').animate({
+            scrollTop: $(`#${name}`).offset().top
+        }, this.t);
+    }
+}
+new Scroll_to_anchore()
